e82c9533a2d7116931ab1a39946da3ee17d123f5
muntasir2165 <muntasir2165@hotmail.com>
2018-06-22 19:52:08 -0400

    added un the unanswered property to the gameInfo state object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
 // every property inside the object is initialized to 0, empty string, or []
 var gameInfo = {"correctAnswerTotal": 0,
 	"incorrectAnswerTotal": 0,
+	"unansweredTotal": 0,
 	"triviaArray": [],
 	"currentQuestionObject": {},
 	"timePerQuestion": 15,
@@ -12,6 +13,7 @@ var gameInfo = {"correctAnswerTotal": 0,
 function initializeGameInfo() {
 	gameInfo = {"correctAnswerTotal": 0,
 	"incorrectAnswerTotal": 0,
+	"unansweredTotal": 0,
 	"triviaArray": [],
 	"currentQuestionObject": {},
 	"timePerQuestion": 15,
@@ -21,7 +23,6 @@ function initializeGameInfo() {
 }
 
 $(document).ready(function() {
-	// var bakcgroundImageIndex = 0;
 	$("#start-game").on("click", function(){
 		displayStartButton(false);
 		displayGameInfo(true);
@@ -67,6 +68,7 @@ function displayGameFeedback(condition, feedback) {
 function updateGameStatistics() {
 	$("#correct-answer-total").text(gameInfo["correctAnswerTotal"]);
 	$("#questions-remaining").text(gameInfo["triviaArray"].length);
+	$("#unanswered-total").text(gameInfo["unansweredTotal"]);
 	$("#incorrect-answer-total").text(gameInfo["incorrectAnswerTotal"]);
 }
 
@@ -80,9 +82,7 @@ function startTrivia() {
 	$.ajax({
 		url: triviaQuestionUrl,
 		success: function(result) {
-			// console.log(result);
 			var triviaDataArray = result["results"];
-			// console.log(triviaDataArray);
 			//intialize the questionArray property variable of gameInfo to be an empty 
 			// array if it wasn't empty already
 			gameInfo.triviaArray = [];
@@ -92,14 +92,9 @@ function startTrivia() {
 			trivia["question"] = triviaData["question"];
 			trivia["correct_answer"] = triviaData["correct_answer"];
 			trivia["incorrect_answers"] = triviaData["incorrect_answers"];
-			// console.log(trivia);
 			gameInfo["triviaArray"].push(trivia);
-			// console.log(gameInfo["triviaArray"]);
 			});
-			// console.log(gameInfo["triviaArray"]);
-			// gameInfo["triviaArray"].forEach(function(question) {
-			// 	console.log(question);
-			// });
+
 			playGame();
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -129,7 +124,7 @@ function countDownTimer() {
 		displayTimeLeft();
 	} else {
 		displayTimeLeft("-");
-		incrementIncorrectAnswerTotalCount();
+		incrementUnAnsweredTotalCount();
 		playGame();
 	}
 }
@@ -149,7 +144,7 @@ function displayTimeLeft(text) {
 }
 
 function resetTimer() {
-	gameInfo.timeLeft = 15;
+	gameInfo.timeLeft = gameInfo.timePerQuestion;
 }
 
 function triviaQuestion() {
@@ -160,93 +155,28 @@ function triviaQuestion() {
 		displayTrivia(gameInfo["currentQuestionObject"]);
 	} else {
 		displayGameInfo(false);
-		// alert("Total Correct Answers: " + gameInfo.correctAnswerTotal + " \n\n" +
-			// "Total Incorrect Answers: " + gameInfo.incorrectAnswerTotal);
-		displayGameFeedback(true, "Total Correct Answers: " + gameInfo.correctAnswerTotal + " and " + 
-			"Total Incorrect Answers: " + gameInfo.incorrectAnswerTotal);
+		displayGameFeedback(true, "Correct Answers: " + gameInfo.correctAnswerTotal + "; Unanswered: " + gameInfo.unansweredTotal + "; Incorrect Answers: " + gameInfo.incorrectAnswerTotal);
 		initializeGameInfo();
-		// setTimeout(displayGameFeedback, 5 * 1000, false);
 		displayStartButton(true);
 	}
 }
-	// function to change the game background automatically every 5 seconds
-	function changeBackground() {
-		var bakcgroundImageArray = ["numbers-1.jpg",
-			"numbers-10.jpg",
-			"numbers-11.jpg",
-			"numbers-12.jpg",
-			"numbers-13.png",
-			"numbers-14.jpg",
-			"numbers-15.jpg",
-			// "numbers-16.jpg",
-			// "numbers-17.jpg",
-			// "numbers-18.jpg",
-			// "numbers-19.jpeg",
-			"numbers-2.jpeg",
-			// "numbers-20.png",
-			// "numbers-21.png",
-			// "numbers-22.jpg",
-			// "numbers-23.jpeg",
-			// "numbers-24.jpg",
-			// "numbers-25.jpg",
-			// "numbers-26.jpg",
-			// "numbers-27.jpg",
-			// "numbers-28.jpeg",
-			// "numbers-29.jpg",
-			"numbers-3.jpg",
-			// "numbers-30.jpeg",
-			"numbers-4.jpeg",
-			"numbers-5.jpeg",
-			"numbers-6.jpeg",
-			"numbers-7.jpg",
-			"numbers-8.png",
-			"numbers-9.jpg"
-		];
-			if (bakcgroundImageIndex >= bakcgroundImageArray.length) {
-				bakcgroundImageIndex = 0;
-			}
-		$('#body').css("background-image", "url(assets/images/" + bakcgroundImageArray[bakcgroundImageIndex] + ")");
-		bakcgroundImageIndex++;
-	}
 
 	// display trivia question on the screen
 	function displayTrivia(questionObject) {
-		console.log("Executing the displayTrivia() function");
+		// console.log("Executing the displayTrivia() function");
 		displayQuestion(questionObject["question"]);
 		displayAnswerChoices(questionObject["incorrect_answers"], questionObject["correct_answer"]);
 	}
 
 	function displayQuestion(question) {
 		// console.log("Executing the displayQuestion() function");
-		// console.log(question);
 		var questionContainer = $("#question-container");
-		// console.log(questionContainer);
 		// questionContainer.empty();
 		questionContainer.html(question);
 		// question.append(gameInfo.firstNumber + gameInfo.operator + gameInfo.secondNumber);
 	}
 
 	function displayAnswerChoices(wrongAnswerChoiceArray, rightAnswer) {
-		/*
-		var answerChoiceArray = wrongAnswerChoiceArray.slice();
-
-		var answerChoicesContainer = $("#answer-choices-container");
-		answerChoicesContainer.empty(); 
-		$.each(answerChoiceArray, function (index, answer) {
-			var answerButton = $("<button>");
-			answerButton.addClass("answer-choice btn btn-info");
-			answerButton.attr("data-answer", answer);
-			answerButton.text(answer);
-			answerChoicesContainer.append(answerButton);
-			answerChoicesContainer.append($("<br>")).append($("<br>"));
-		});
-		var answerButton = $("<button>");
-			answerButton.addClass("answer-choice btn btn-success");
-			answerButton.attr("data-answer", rightAnswer);
-			answerButton.text(rightAnswer);
-			answerChoicesContainer.append(answerButton);
-			answerChoicesContainer.append($("<br>")).append($("<br>"));
-		*/
 		// console.log("Executing the displayAnswerChoices() function");
 		var answerChoiceArray = wrongAnswerChoiceArray.slice();
 		answerChoiceArray.push(rightAnswer);
@@ -274,12 +204,9 @@ function evaluateClickedAnswer() {
 		displayTimeLeft("-");
 		var answer = $(this).attr("data-answer");
 		console.log("answer: " + answer);
-		// console.log(gameInfo["currentQuestionObject"]["correct_answer"]);
 		if (answer === gameInfo["currentQuestionObject"]["correct_answer"]) {
-			// console.log("Correct Answer!");
 			incrementCorrectAnswerTotalCount();
 		} else {
-			// console.log("Incorrect Answer!");
 			incrementIncorrectAnswerTotalCount();
 		}
 		updateGameStatistics();
@@ -293,6 +220,10 @@ function incrementCorrectAnswerTotalCount() {
 
 function incrementIncorrectAnswerTotalCount() {
 	gameInfo["incorrectAnswerTotal"]++;
+}
+
+function incrementUnAnsweredTotalCount() {
+	gameInfo["unansweredTotal"]++;
 }
 
 	// Fisher-Yates (aka Knuth) Shuffle algorithm implementation to shuffle trivia question answer choices
